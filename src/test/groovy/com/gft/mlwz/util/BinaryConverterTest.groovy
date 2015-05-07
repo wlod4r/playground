1d70f207afdca9b358fe2e4e740439c9622b77de
Michal Wlodarczyk <Michal.Wlodarczyk@rulefinancial.com>
2015-05-07 12:26:03 +0200

    Binary converter test extended

--- src/test/groovy/com/gft/mlwz/util/BinaryConverterTest.groovy
+++ src/test/groovy/com/gft/mlwz/util/BinaryConverterTest.groovy
@@ -2,6 +2,7 @@ package com.gft.mlwz.util
 
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class BinaryConverterTest extends Specification {
 
@@ -12,11 +13,34 @@ class BinaryConverterTest extends Specification {
         underTest = new BinaryConverter()
     }
 
-    def "should convert int to binary value"() {
+    @Unroll
+    def "should convert #i to binary value #b"() {
         expect:
             underTest.convertInt(i) == b
         where:
             i << [0, 1, 2, 3, 4, 5, 6, 7, 8]
             b << ["0", "1", "10", "11", "100", "101", "110", "111", "1000"]
+    }
+
+    @Unroll
+    def "should convert int #i to binary value #b second approach"() {
+        expect:
+            underTest.convertInt(i) == b
+        where:
+            i | b
+            0 | "0"
+            1 | "1"
+            2 | "10"
+            3 | "11"
+            4 | "100"
+            5 | "101"
+            6 | "110"
+            7 | "111"
+            8 | "1000"
+    }
+
+    def "should convert single int value to binary representation"() {
+        expect:
+            underTest.convertInt(16) == "10000"
     }
 }
